bff5db36c6dcb9b407a697a4ca54b953e372b8d9
cactus-denver <andrew@cactusdenver.com>
2017-12-08 16:26:00 -0700

    Grit Going back live after youatCollege

---
+++ grit/newsroom/index.php
@@ -0,0 +1,227 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Grit Digital Health | NEWSROOM</title>
+    <meta property="og:title" content="Grit Digital Health | Press Kit">
+    <meta property="og:site_name" content="Grit Digital Health">
+    <meta property="og:url" content="http://gritdigitalhealth.com/press-kit/">
+    <meta property="og:description" content="A human-centric business establishing a new set of norms for the ways people address well-being through tools and through each other.">
+    <meta property="og:type" content="business">
+    <meta property="og:image" content="http://gritdigitalhealth.com/img/product-hero-inline.jpg">
+    <link rel="icon" href="../favicon/favicon.ico" type="image/x-icon"/>
+    <link rel="apple-touch-icon" sizes="57x57" href="../favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="../favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../favicon/ms-icon-144x144.png">
+	<link rel="stylesheet" type="text/css" href="../css/screen.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="../js/imagesloaded.js" type="text/javascript"></script>
+    <script src="../js/min/jquery.smoothState-min.js" type="text/javascript"></script>
+    <script src="../js/tracking.js" type="text/javascript"></script>
+    <script src="../js/script.js" type="text/javascript"></script>
+    </head>
+<body>
+    <div id="main" class="hidden">
+        <header>
+            <nav>
+            <ul class="desktop">
+                <li class="text-link"><a href="../product/" data-target="-2"><span>PRODUCT</span></a></li>
+                <li class="text-link"><a href="../team/" data-target="-1">TEAM</a></li>
+                <li ><a href="../" data-target="0"><img src="../img/GritLogo.svg" alt="grit logo"/></a></li>
+                <li class="text-link"><a href="../index.php#about" data-target="0">ABOUT</a></li>
+                <li class="text-link contact-link"><a href="../contact/" data-target="1">CONTACT</a></li>
+            </ul>
+            </nav>
+            <a href="../index.php" class="mobile logo"><img src="../img/GritLogo.svg" alt="grit logo"/></a>
+            <div id="nav-icon" class="mobile">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <nav class="mobile">
+                <ul>
+                    <li><a href="../index.php">HOME</a></li>
+                    <li><a href="../product/">PRODUCT</a></li>
+                    <li><a href="../team/">TEAM</a></li> 
+                    <li><a href="../index.php#about">ABOUT</a></li>
+                    <li><a href="../contact/" class="contact-link">CONTACT</a></li> 
+                </ul>
+            </nav>
+        </header>
+        <div class="sceneElement" data-viewport="-2">            
+        <section class="press-feature hero">
+          	<div class="content-wrap">
+                    <div class="spacer"></div>
+                    <h4>NEWSROOM</h4>
+                    <p>YOU is the hub for student well-being on campus, created by Grit Digital Health in collaboration with universities to address the well-being crisis facing every student and campus. YOU centralizes, optimizes and expands university resources and connects students with personalized content curated and vetted to help them navigate life and campus. The interactive platform – customized for both the student and the campus – takes a holistic and proactive approach to student success and well-being.</p>
+            </div>
+        </section> 
+         <section class="press-feature">
+           	<div class="content-wrap">
+                <div class="spacer"></div>
+                <h4>Press Releases</h4>
+                 <div class="item-wrap">
+                    <h5>January 18, 2017</h5>
+                    <p>'YOU at College' Unveils Major Product Update to Better Serve Today's Diverse Campus Populations</p>
+                    <a href="http://www.marketwired.com/press-release/you-college-unveils-major-product-update-better-serve-todays-diverse-campus-populations-2189335.htm" target="_blank" class="button">Read the Press Release</a>
+                </div>
+                <div class="item-wrap">
+                    <h5>November 30, 2016</h5>
+                    <p>Grit Digital Health Awarded NASPA and Gold Leaf Distinctions</p>
+                    <a href="http://www.gritdigitalhealth.com/assets/November_2016_Awards_Press_Release.pdf" target="_blank" class="button">Read the Press Release</a>
+                </div>
+                <div class="item-wrap">
+                    <h5>September 7, 2016</h5>
+                    <p>Student Wellness Portal Launched to Address the Well-being Crisis on Campus</p>
+                    <a href="http://www.marketwired.com/press-release/student-wellness-portal-launched-to-address-the-well-being-crisis-on-campus-2156069.htm" target="_blank" class="button">Read the Press Release</a>
+                </div>
+            </div>
+     	  </section>
+          
+          <section class="press-feature news">
+          	<div class="content-wrap">
+                  <div class="spacer"></div>
+                  <h4>YOU in the News</h4>
+                  <div>
+                  <div class="item-wrap">
+					  <h5>University Business <span>“The Hidden Crisis on Campus”</span></h5>
+                    <span>November 14, 2016</span>
+                    <p>Grit Digital Health's Director of Clinical Programs, Nathaan Demers, Psy.D., explains how technology can be used beyond subsidizing therapy sessions to reach otherwise silently struggling students in this piece published by University Business.</p>
+                    <a href="https://www.universitybusiness.com/article/1216-crisis" target="_blank" class="button">Read Article</a>
+                  </div>
+                  <div class="item-wrap">
+					  <h5>Inside Higher Ed <span>"A Virtual Counselor"</span></h5>
+                    <span>October 19, 2016</span>
+                    <p>With rising counseling center demands, online platforms like the YOU@CSU portal are helping counseling centers by going upstream to connect students to the right resources at the right time.</p>
+                    <a href="https://www.insidehighered.com/news/2016/10/19/colleges-turn-online-text-messaging-services-help-counseling-demand" target="_blank" class="button">Read Article</a>
+                  </div>
+                  <div class="item-wrap">
+					  <h5>Suicide Prevention Social Media Webcast <span>"Tech Integration in Suicide Prevention Panel"</span></h5>
+                    <span>September 25, 2016</span>
+                    <p>A live panel webcast on SPSMchat.com discussing the integration of digital technologies in suicide prevention featuring Nathaan Demers, Psy.D. - the Grit Digital Health Director of Clinical Programs.</p>
+                    <a href="https://spsmchat.com/2016/09/13/doc_demers-joins-an-all-star-spsm-panel-to-chat-about-tech-integration-in-suicide-prevention-92516-8pct/" target="_blank" class="button">Read Article</a>
+                  </div>
+                  <div class="item-wrap">
+					  <h5>MHealth Intelligence <span>"Using MHealth to Connect With Students in Crisis"</span></h5>
+                    <span>September 19, 2016</span>
+                    <p>A demonstration of how colleges across the country including Colorado State University, are using digital health platforms like the YOU student well-being portal to help students access behavioral health services.</p>
+                    <a href="http://mhealthintelligence.com/news/using-mhealth-to-connect-with-students-in-crisis" target="_blank" class="button">Read Article</a>
+                  </div>
+                  <div class="item-wrap">
+					  <h5>University Business <span>"Student Wellness Portal Launched"</span></h5>
+                    <span>September 10, 2016</span>
+                    <p>University Business's coverage of the YOU portal launch.</p>
+                    <a href="https://www.universitybusiness.com/news/student-wellness-portal-launched" target="_blank" class="button">Read Article</a>
+                  </div>
+                  <div class="item-wrap">
+					  <h5>The Collegian <span>"Student Demand for Counseling Services Increase, CSU Health Network Expands"</span></h5>
+                    <span>August 31, 2016</span>
+                    <p>An article from The Collegian at Colorado State University detailing how the counseling center is meeting student demand and leveraging the power of YOU@CSU to do so.</p>
+                    <a href="http://collegian.com/2016/08/student-demand-for-counseling-services-increase-csu-health-network-expands/" target="_blank" class="button">Read Article</a>
+                  </div>
+                  <div class="item-wrap">
+					  <h5>Disruptive Women in Healthcare <span>"Finding Commonplace on Digital Spaces for Campus Well-being"</span></h5>
+                    <span>August 16, 2016</span>
+                    <p>YOU@CSU featured in Anne Hudgens’s post on the Disruptive Women in Health Care blog.</p>
+                    <a href="http://www.disruptivewomen.net/2016/08/16/finding-commonplace-on-digital-spaces-for-campus-well-being/" target="_blank" class="button">Read Article</a>
+                  </div>
+                  <div class="item-wrap">
+					  <h5>Around the Oval <span>"Communicators with a Purpose"</span></h5>
+                    <span>Spring 2016</span>
+                    <p>Announcement of the soft launch of YOU@CSU in Around the Oval, CSU’s semi-annual magazine published by the CSU Alumni Association.</p>
+                    <a href="https://webcms.colostate.edu/alumni/media/sites/85/2016/04/ATO_Spring_2016-web.pdf" target="_blank" class="button">Read Article</a>
+                  </div>
+				  </div>
+              </div>                  
+          </section>
+          
+          <section class="press-feature">
+          	<div class="content-wrap">
+              <div class="spacer"></div>
+           	  <h4>Multimedia</h4>
+              <div class="item-wrap">
+                <h5>Grit Logos</h5>
+                <p>Click below to download the Grit Digital Health logo in a variety of formats. These logos should not be altered or adjusted in any way.</p>
+                <a href="../img/GRIT_Logo-4C.zip" target="_blank" class="button">Download Zip File</a>
+              </div>
+              <div class="item-wrap">
+                <h5>Student Well-being Video</h5>
+                <p>The YOU video, linked below, highlights the background and mission that fuels our work.</p>
+                <a href="https://vimeo.com/205067777" target="_blank" class="button">Watch the Video</a>
+              </div>
+             </div>
+          </section>            
+
+          <section class="press-feature">
+          	<div class="content-wrap">
+              <div class="spacer"></div>
+           	  <h4>Awards</h4>
+           	    <div class="item-wrap">
+              	<h5>Colorado Healthcare Communicators Gold Leaf Awards</h5>
+                <span>November 2016</span>
+                <p>Grit Digital Health was recently honored with two awards at the Colorado Healthcare Communicators’ Gold Leaf Awards. Grit took home a Silver Leaf Award in the website category for the YOU@CSU college student well-being portal and a Bronze Leaf Award in the category of print/digital/outdoor campaign in a single format for the “Help is Here” posters promoting the YOU portal on the CSU campus.</p>
+              </div>
+              <div class="item-wrap">
+              	<h5>NASPA region IV-West Video/Computer Software Publications Award</h5>
+                <span>November 2016</span>
+                <p>YOU@CSU, the premier instance of the YOU portal for a campus developed in partnership with Colorado State University, recently earned the NASPA region IV-West Video/Computer Software Publications Award. The receipt of this award substantiates the importance and power of technology to solve the ever-present challenge of connecting with each and every student in a unique and personal way.</p>
+              </div>
+            </div>
+          </section>            
+
+                   
+   		<footer>
+            <div class="newsletter">
+                <h4>Keep me up-to-date</h4>
+    
+                <!-- Begin MailChimp Signup Form -->
+                <div id="mc_embed_signup">
+                <form action="//gritdigitalhealth.us12.list-manage.com/subscribe/post?u=32f42af3fbd53a0a8a93a3340&amp;id=c40b190c68" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+                    
+                <div class="mc-field-group">
+                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Enter your email">
+                </div>
+                    <div id="mce-responses" class="clear">
+                        <div class="response" id="mce-error-response" style="display:none"></div>
+                        <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_32f42af3fbd53a0a8a93a3340_c40b190c68" tabindex="-1" value=""></div>
+                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                    </div>
+                </form>
+                </div>
+                <!--End mc_embed_signup-->
+            </div>
+            <ul>
+                <li class="text-link"><a href="">PRODUCT</a></li>
+                <li class="text-link"><a href="../team">TEAM</a></li>
+                <li class="text-link"><a href="../index.php#about">ABOUT</a></li>
+                <li class="text-link"><a href="../contact">CONTACT</a></li>
+            	<li class="text-link"><a href="../newsroom">NEWSROOM</a></li>
+            </ul>  
+            <p>© <?php echo date("Y") ?> Grit Digital Health, LLC</p>
+      </footer>
+     </div>      
+     </div> 
+      <div class="loader">
+            <div class="wrap">
+                <div class="load-slider"></div>
+            </div>
+      </div>
+  </body>
+</html>
