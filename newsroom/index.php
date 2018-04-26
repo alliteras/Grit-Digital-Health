c9ee22b8f7af34038b3ae54bcb8f214b53c89c95
cactus-denver <andrew@cactusdenver.com>
2018-04-26 16:16:00 -0600

    Newsroom updates

--- newsroom/index.php
+++ newsroom/index.php
@@ -1,16 +1,21 @@
    	<?php include '../build/header.php';?>
-        <div class="sceneElement" data-viewport="-2">            
+        <div class="sceneElement" data-viewport="-2">
         <section class="press-feature hero">
           	<div class="content-wrap">
                     <div class="spacer"></div>
                     <h4>NEWSROOM</h4>
                     <p>YOU is the hub for student well-being on campus, created by Grit Digital Health in collaboration with universities to address the well-being crisis facing every student and campus. YOU centralizes, optimizes and expands university resources and connects students with personalized content curated and vetted to help them navigate life and campus. The interactive platform – customized for both the student and the campus – takes a holistic and proactive approach to student success and well-being.</p>
             </div>
-        </section> 
+        </section>
          <section class="press-feature">
            	<div class="content-wrap">
                 <div class="spacer"></div>
                 <h4>Press Releases</h4>
+                <div class="item-wrap">
+                   <h5>April 20, 2018</h5>
+                   <p>Hopelab And Grit Digital Health Join Forces To Tackle The Rapid Rise Of Loneliness Affecting College-Aged Youth In The U.S.</p>
+                   <a href="https://www.prnewswire.com/news-releases/hopelab-and-grit-digital-health-join-forces-to-tackle-the-rapid-rise-of-loneliness-affecting-college-aged-youth-in-the-us-300626620.html?tc=eml_cleartime" target="_blank" class="button">Read the Press Release</a>
+               </div>
                  <div class="item-wrap">
                     <h5>January 18, 2017</h5>
                     <p>'YOU at College' Unveils Major Product Update to Better Serve Today's Diverse Campus Populations</p>
@@ -28,72 +33,92 @@
                 </div>
             </div>
      	  </section>
-          
+
           <section class="press-feature news">
           	<div class="content-wrap">
                   <div class="spacer"></div>
                   <h4>YOU in the News</h4>
                   <div>
+                    <div class="item-wrap">
+  					<h5>APA Monitor - <span>“Necessity Led to Inventions From These Psychologists”</span></h5>
+                      <span>April 1, 2018</span>
+                      <p>Nathaan Demers explains how YOU at College employs technology to go upstream and overcome limitations of the college counseling center. </p>
+                      <a href="http://www.apamonitor-digital.org/apamonitor/201804/MobilePagedArticle.action?articleId=1366753#articleId1366753" target="_blank" class="button">Read Article</a>
+                    </div>
+                    <div class="item-wrap">
+  					<img src="../img/USA-Today_college.png" />
+  					<h5>USA Today College<span>“More and more students need mental health services. But colleges struggle to keep up.”</span></h5>
+                      <span>May 4, 2017</span>
+                      <p>Colleges across the country are working to adapt to growing mental health needs as demand outpaces traditional approaches. </p>
+                      <a href="http://college.usatoday.com/2017/05/04/more-and-more-students-need-mental-health-services-but-colleges-struggle-to-keep-up/" target="_blank" class="button">Read Article</a>
+                    </div>
                   <div class="item-wrap">
-					<img src="../img/logo.jpg" />  
+					<img src="../img/logo.jpg" />
 					<h5>University Business <span>“The Hidden Crisis on Campus”</span></h5>
                     <span>November 14, 2016</span>
                     <p>Grit Digital Health's Director of Clinical Programs, Nathaan Demers, Psy.D., explains how technology can be used beyond subsidizing therapy sessions to reach otherwise silently struggling students in this piece published by University Business.</p>
                     <a href="https://www.universitybusiness.com/article/1216-crisis" target="_blank" class="button">Read Article</a>
                   </div>
                   <div class="item-wrap">
-					<img src="../img/ihe-logo-2018.jpg"/>  
+					<img src="../img/ihe-logo-2018.jpg"/>
 					<h5>Inside Higher Ed <span>"A Virtual Counselor"</span></h5>
                     <span>October 19, 2016</span>
                     <p>With rising counseling center demands, online platforms like the YOU@CSU portal are helping counseling centers by going upstream to connect students to the right resources at the right time.</p>
                     <a href="https://www.insidehighered.com/news/2016/10/19/colleges-turn-online-text-messaging-services-help-counseling-demand" target="_blank" class="button">Read Article</a>
                   </div>
                   <div class="item-wrap">
-					<img src="../img/SPSM-logo.jpg"/>  
+					<img src="../img/SPSM-logo.jpg"/>
 					<h5>Suicide Prevention Social Media Webcast <span>"Tech Integration in Suicide Prevention Panel"</span></h5>
                     <span>September 25, 2016</span>
                     <p>A live panel webcast on SPSMchat.com discussing the integration of digital technologies in suicide prevention featuring Nathaan Demers, Psy.D. - the Grit Digital Health Director of Clinical Programs.</p>
                     <a href="https://spsmchat.com/2016/09/13/doc_demers-joins-an-all-star-spsm-panel-to-chat-about-tech-integration-in-suicide-prevention-92516-8pct/" target="_blank" class="button">Read Article</a>
                   </div>
                   <div class="item-wrap">
-					<img src="../img/mHealth.jpg" />  
+					<img src="../img/mHealth.jpg" />
 					<h5>MHealth Intelligence <span>"Using MHealth to Connect With Students in Crisis"</span></h5>
                     <span>September 19, 2016</span>
                     <p>A demonstration of how colleges across the country including Colorado State University, are using digital health platforms like the YOU student well-being portal to help students access behavioral health services.</p>
                     <a href="http://mhealthintelligence.com/news/using-mhealth-to-connect-with-students-in-crisis" target="_blank" class="button">Read Article</a>
                   </div>
                   <div class="item-wrap">
-					<img src="../img/logo.jpg" />  
+					<img src="../img/logo.jpg" />
 					 <h5>University Business <span>"Student Wellness Portal Launched"</span></h5>
                     <span>September 10, 2016</span>
                     <p>University Business's coverage of the YOU portal launch.</p>
                     <a href="https://www.universitybusiness.com/news/student-wellness-portal-launched" target="_blank" class="button">Read Article</a>
                   </div>
                   <div class="item-wrap">
-					<img src="../img/Collegian-logo.png" />  
+					<img src="../img/Collegian-logo.png" />
 					<h5>The Collegian <span>"Student Demand for Counseling Services Increase, CSU Health Network Expands"</span></h5>
                     <span>August 31, 2016</span>
                     <p>An article from The Collegian at Colorado State University detailing how the counseling center is meeting student demand and leveraging the power of YOU@CSU to do so.</p>
                     <a href="http://collegian.com/2016/08/student-demand-for-counseling-services-increase-csu-health-network-expands/" target="_blank" class="button">Read Article</a>
                   </div>
                   <div class="item-wrap">
-					<img src="../img/Disruptive-Women.png" />  
+					<img src="../img/Disruptive-Women.png" />
 					<h5>Disruptive Women in Healthcare <span>"Finding Commonplace on Digital Spaces for Campus Well-being"</span></h5>
                     <span>August 16, 2016</span>
                     <p>YOU@CSU featured in Anne Hudgens’s post on the Disruptive Women in Health Care blog.</p>
                     <a href="http://www.disruptivewomen.net/2016/08/16/finding-commonplace-on-digital-spaces-for-campus-well-being/" target="_blank" class="button">Read Article</a>
                   </div>
                   <div class="item-wrap">
-					<img src="../img/CSU-logo.png" />  
+					<img src="../img/XM-logo.png" />
+					<h5>Sirius XM<span>"Top of Mind With Julie Rose"</span></h5>
+                    <span>August 15, 2017</span>
+                    <p>ive years after launch, Dr. Rich Mahogany is still provocatively and proactively addressing mental health for working-age men - with the results to prove it.  Grit Digital Health Founder and CEO Joe Conrad discusses Man Therapy and the strategy behind the mustache.</p>
+                    <a href="https://vimeo.com/239734253/f49c7996ab" target="_blank" class="button">Read Article</a>
+                  </div>
+                  <div class="item-wrap">
+					<img src="../img/CSU-logo.png" />
 					<h5>Around the Oval <span>"Communicators with a Purpose"</span></h5>
                     <span>Spring 2016</span>
                     <p>Announcement of the soft launch of YOU@CSU in Around the Oval, CSU’s semi-annual magazine published by the CSU Alumni Association.</p>
                     <a href="https://webcms.colostate.edu/alumni/media/sites/85/2016/04/ATO_Spring_2016-web.pdf" target="_blank" class="button">Read Article</a>
                   </div>
 				  </div>
-              </div>                  
+              </div>
           </section>
-          
+
           <section class="press-feature">
           	<div class="content-wrap">
               <div class="spacer"></div>
@@ -109,7 +134,7 @@
                 <a href="https://vimeo.com/205067777" target="_blank" class="button">Watch the Video</a>
               </div>
              </div>
-          </section>            
+          </section>
 
           <section class="press-feature">
           	<div class="content-wrap">
@@ -126,5 +151,5 @@
                 <p>YOU@CSU, the premier instance of the YOU portal for a campus developed in partnership with Colorado State University, recently earned the NASPA region IV-West Video/Computer Software Publications Award. The receipt of this award substantiates the importance and power of technology to solve the ever-present challenge of connecting with each and every student in a unique and personal way.</p>
               </div>
             </div>
-          </section>            
+          </section>
 	<?php include '../build/footer.php';?>
